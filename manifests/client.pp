338b4a5206a527a4a1b567020593e69304aeec91
Paul Belanger <paul.belanger@polybeacon.com>
2013-02-03 14:09:08 -0500

    Initial import
    Change-Id: I74feb76f6b02990a050537c4e2ae56d8f5bc1879
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ manifests/client.pp
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2011-2013, Polybeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+class pbuilder::client {
+  # TODO add ccache to kickstandproject
+  # require ccache::client
+}
+
+# vim:sw=2:ts=2:expandtab
---
+++ manifests/params.pp
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2011, Polybeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+class pbuilder::params {
+  $cachedir = $::operatingsystem ? {
+    default => '/var/cache/pbuilder',
+  }
+
+  $configdir = $::operatingsystem ? {
+    default => '/etc/pbuilder',
+  }
+
+  $configfile = $::operatingsystem ? {
+    default => '/etc/pbuilderrc',
+  }
+
+  $group = $::operatingsystem ? {
+    default => 'root',
+  }
+
+  $mode = $::operatingsystem ? {
+    default => '0644',
+  }
+
+  $owner = $::operatingsystem ? {
+    default => 'root',
+  }
+
+  # TODO common module?
+  $localbindir = $::operatingsystem ? {
+    default => '/usr/local/bin',
+  }
+
+  $packagename = $::operatingsystem ? {
+    default => 'pbuilder',
+  }
+}
+
+# vim:sw=2:ts=2:expandtab
